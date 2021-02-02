9bbce82f05b999e51537a1944ee68f52003af3b9
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2021-02-01 16:23:41 -0800

    Update main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -14,7 +14,6 @@ enum Directions {
     //% block="right" 
     RIGHT 
 }
-
 /**
  * Custom blocks
  */
--- test.ts
+++ test.ts
@@ -1,1 +1,40 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+// ***************************************
+// *
+// * demonstrates use of blocks
+// * by simulating a certain series of "moves". 
+// * note: final display after this sequence 
+// * will vary because mazes are created
+// * by a random process and will
+// * be different each time the code runs. 
+// * Run this code a few times to see some variety. 
+// * Keep in mind that the randomness applies 
+// * only during the creation of the maze; however,
+// * the maze does not change during play
+// * while the player is moving around in it.
+// *
+// *****************************************
+
+// maximum diension reporter block
+basic.showNumber(maze.maximumDimension());
+
+// create new maze, using minimum dimensions 
+maze.newMaze(2, 2);
+// pause to see the Entrance cell displayed
+basic.pause(2000);
+
+// turn on the breadcrumbs display setting 
+maze.displayCrumbs(Crumbstatus.ON);
+
+// make six moves: right, down, up, down, right, right 
+// might end showing the Exit cell, might not!
+maze.move(Directions.RIGHT); // enter upper-left corner cell in the maze
+basic.pause(2000);
+maze.move(Directions.DOWN); // attempt move to lower-left cell 
+basic.pause(2000);
+maze.move(Directions.UP); // try re-entering the upper-left cell 
+basic.pause(2000); // breadcrumb might show up now
+maze.move(Directions.DOWN); // back to lower-left cell 
+maze.move(Directions.RIGHT); // attempt move to lower-right cell 
+maze.move(Directions.RIGHT); // attempt move to Exit cell
+
+
